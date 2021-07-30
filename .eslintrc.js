49830499dee57f6fbef971b01443149f37648bb3
liyiyuan <841353917@qq.com>
2021-07-30 11:16:34 +0800

    忽略eslint下划线命名错误提示

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['./react-native'],
+  extends: ['./base.js'],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
--- overrides/rules/javascript.js
+++ overrides/rules/javascript.js
@@ -2,6 +2,7 @@ module.exports = {
   'no-use-before-define': 0, // 禁止定义前使用
   'no-nested-ternary': 0, // 禁止使用嵌套的三元表达式
   'no-unused-expressions': 0, // 禁止未使用过的表达式
+  'no-underscore-dangle': 0, // 禁止下滑线作为起始命名
   'import/prefer-default-export': 0, // When there is only a single export from a module, prefer using default export over named export.
   'import/extensions': 0, // Ensure consistent use of file extension within the import path
   'import/no-extraneous-dependencies': 0, // Forbid the use of extraneous packages
